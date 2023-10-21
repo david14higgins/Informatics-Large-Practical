38b419150d25ac810afa52159023e6346b53732d
David Higgins <david14higgins@gmail.com>
2023-10-21 19:00:13 +0100

    initial commit

---
+++ src/main/java/uk/ac/ed/inf/App.java
@@ -0,0 +1,15 @@
+package uk.ac.ed.inf;
+
+import uk.ac.ed.inf.ilp.constant.OrderStatus;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Small test for the IlpDataObjects");
+
+        var orderStatus = OrderStatus.DELIVERED;
+
+        System.out.println("IlpDataObjects.jar was used");
+    }
+}
