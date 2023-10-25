80b5a643c1d58d58d9b1a555d8c0f53fb1bcdbc8
David Higgins <david14higgins@gmail.com>
2023-10-25 16:58:08 +0100

    system controller structure outlined

--- src/main/java/uk/ac/ed/inf/SystemController.java
+++ src/main/java/uk/ac/ed/inf/SystemController.java
@@ -5,6 +5,7 @@ import uk.ac.ed.inf.ilp.data.LngLat;
 import uk.ac.ed.inf.ilp.data.NamedRegion;
 import uk.ac.ed.inf.ilp.data.Order;
 import uk.ac.ed.inf.ilp.data.Restaurant;
+import uk.ac.ed.inf.ilp.data.Pizza;
 
 import java.util.ArrayList;
 
@@ -24,23 +25,41 @@ public class SystemController {
 
         //Setup OrderValidator and LngLatHandler
         OrderValidator orderValidator = new OrderValidator();
-        LngLatHandler lngLatHandler = new LngLatHandler();
+        LngLat appletonTower = new LngLat(-3.186874, 55.944494);
 
         //Iterate through all fetched orders for that day
         for (Order order : ordersByDate) {
             order = orderValidator.validateOrder(order, restaurants);
             if (order.getOrderStatus() == OrderStatus.VALID_BUT_NOT_DELIVERED) {
                 //Plan drone route to and from restaurant and write to files
+                LngLat restaurantLocation = findRestaurantLocation(order, restaurants);
                 RoutePlanner routePlanner = new RoutePlanner();
-                //ArrayList<MoveRecord> route = routePlanner.planRoute()
-
-
+                ArrayList<MoveRecord> route = routePlanner.planRoute(appletonTower, restaurantLocation, noFlyZones, centralArea);
+                //These moves will need to be written to the JSON files 
             }
 
             //Need to write order to deliveries file regardless of whether the order was delivered
 
         }
-
-
     }
+
+    //Returns the LngLat location of the restaurant in a valid order
+    private static LngLat findRestaurantLocation(Order order, Restaurant[] restaurants) {
+        Pizza firstPizzaInOrder = order.getPizzasInOrder()[0];
+        //Iterate through possible restaurants
+        for (Restaurant restaurant : restaurants) {
+            //Search menu for this pizza
+            for(Pizza pizzaOnMenu : restaurant.menu()) {
+                if (firstPizzaInOrder.name().equals(pizzaOnMenu.name()) &&
+                    firstPizzaInOrder.priceInPence() == pizzaOnMenu.priceInPence()) {
+                    //Pizza found, return restaurant location
+                    return restaurant.location();
+                }
+            }
+        }
+        //This method will only be used on a valid order so the restaurant should be found
+        return null;
+    }
+
+
 }
